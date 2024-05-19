1b3ad960e93de46e3aebae91d23455418302c095
ARAVINDANS25 <130492356+ARAVINDANS25@users.noreply.github.com>
2024-05-19 15:07:29 +0530

    Update My_portfolio.py

--- My_portfolio.py
+++ My_portfolio.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 st.write("##")
-st.subheader("Hey Guys : I'm  Aravindan S")
+st.subheader("Hey Guys , I'm  Aravindan S :wave:")
 st.title("Welcome to My Portfolio Website")
 
