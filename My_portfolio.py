9c628128bb114fc8f3c51c703ee74b094b769098
ARAVINDANS25 <130492356+ARAVINDANS25@users.noreply.github.com>
2024-05-19 15:09:52 +0530

    Update My_portfolio.py

--- My_portfolio.py
+++ My_portfolio.py
@@ -5,4 +5,9 @@ st.set_page_config(layout="wide")
 st.write("##")
 st.subheader("Hey Guys , I'm  Aravindan S :wave:")
 st.title("Welcome to My Portfolio Website")
+st.subheader("About")
+st.write("""As a passionate and dedicated entry-level data professional, 
+I am eager to bring my skills in data analysis, cloud computing, and data management 
+to real-world projects. With certification and five badges in Snowflake, I have developed 
+a strong foundation in data warehousing, ETL processes, and data integration.""")
 
