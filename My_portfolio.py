a5a152f6dc866a6af155b475f4a72346a512f7d8
ARAVINDANS25 <130492356+ARAVINDANS25@users.noreply.github.com>
2024-05-19 23:26:21 +0530

    Update My_portfolio.py

--- My_portfolio.py
+++ My_portfolio.py
@@ -167,3 +167,18 @@ if selected == 'Contacts':
         - [GitHub](https://github.com/ARAVINDANS25)
         - [Email](mailto:aravindans2503@gmail.com)
         """)
+
+
+        st.write("##")
+            st.subheader("Or send me a message directly:")
+        
+            contact_form = """
+            <form action="https://formsubmit.co/aravindans2503@gmail.com" method="POST">
+            <input type="hidden" name="_captcha" value="false">
+            <input type="text" name="name" placeholder="Your name" required>
+            <input type="email" name="email" placeholder="Your email" required>
+            <textarea name="message" placeholder="Your message here" required></textarea>
+            <button type="submit">Send</button>
+            </form>
+            """
+            st.markdown(contact_form, unsafe_allow_html=True)
